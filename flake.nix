4fb0bb4605b9ac53488ef2aa8ec082e43efc63dc
Andreas Lösel <andreas.loesel@outlook.com>
2026-09-19 10:04:05 +0200

    Add nix flake with dev tools for compiling translations

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "WeatherLockscreen dev environment (lua5.1 for syntax checks, gettext for translations)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.buildEnv {
+          name = "weatherlockscreen-tools";
+          paths = [ pkgs.lua5_1 pkgs.gettext pkgs.bash ];
+        };
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          name = "weatherlockscreen-dev";
+          buildInputs = [ pkgs.lua5_1 pkgs.gettext pkgs.bash ];
+          shellHook = ''
+            echo "WeatherLockscreen dev shell: luac $(luac -v 2>&1 | head -n1), $(msgfmt --version | head -n1)"
+          '';
+        };
+      });
+    };
+}
